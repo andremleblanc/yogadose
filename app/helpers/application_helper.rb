5ea39a17e96d06c2709760401ad0c26a629e1593
Andre LeBlanc <andre.leblanc@accelerated.com>
2016-11-20 23:11:15 -0500

    progress on login forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def neon_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="form-login-error">
+      <h3>#{sentence}</h2>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end

c06c90cb39f3c8941f7df58a52aaedc858ed004b
Andre LeBlanc <andre.leblanc@accelerated.com>
2017-02-06 22:31:30 -0500

    feat: authenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
-  root to: 'pages#show', id: 'home'
-  get 'account', to: 'dashboard#show', as: :account
-  get 'dashboard', to: 'dashboard#show', as: :dashboard
-  get "/pages/*id" => 'pages#show', as: :page, format: false
-
-  devise_for :users, :controllers => {
-    registrations: 'users/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
-
   resources :routines
   resources :subscriptions, only: %i(new create edit)
   resources :users, only: :index
+
+  get 'account', to: 'dashboard#show', as: :account
+  get 'dashboard', to: 'dashboard#show', as: :dashboard
+
+  # Static Pages Routes
+  get "/pages/*id" => 'pages#show', as: :page, format: false
+
+  devise_for :users, :controllers => {
+      registrations: 'users/registrations',
+      omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  # Authenticated Root
+  authenticated do
+    root to: 'dashboard#show', as: :authenticated_root
+  end
+
+  # Unauthenticated Root
+  root to: 'pages#show', id: 'home'
 end
---
+++ spec/requests/root_path_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Root path', type: :request do
+  context 'when logged in' do
+    it 'renders dashboard' do
+      user = create(:user)
+      sign_in user
+      get root_path
+      expect(response).to render_template(:show)
+    end
+  end
+
+  context 'when not logged in' do
+    it 'redirects to home page' do
+      get root_path
+      expect(response).to render_template(:home)
+    end
+  end
+end

efb4238c6c8c74fbe6faf6f2c3b4302220dae350
Andre LeBlanc <andre.leblanc@accelerated.com>
2017-02-11 22:54:43 -0500

    change password

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+      registrations: 'users/registrations',
+      omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   resources :routines
   resources :subscriptions, only: %i(new create edit)
   resources :users, only: [ :index, :edit ]
 
   get 'account', to: 'users#edit', as: :account
   get 'dashboard', to: 'dashboard#show', as: :dashboard
-
-  devise_for :users, :controllers => {
-      registrations: 'users/registrations',
-      omniauth_callbacks: 'users/omniauth_callbacks'
-  }
 
   # Authenticated Root
   authenticated do

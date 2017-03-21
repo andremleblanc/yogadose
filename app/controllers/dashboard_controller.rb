8bbec9b63efea325e66ff39771dac5adb0aceb7b
Andre LeBlanc <andre.leblanc@accelerated.com>
2017-03-20 23:51:43 -0400

    wp

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
   def show
-    #TODO: authorize video access
+    @media = JSON.parse(Net::HTTP.get(URI('https://cdn.jwplayer.com/v2/playlists/lnBWhBPe')))['playlist']; # TODO: Cache and AJAX
   end
 end
